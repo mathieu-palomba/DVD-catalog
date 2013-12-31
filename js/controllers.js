714eb0241d5cd48878d94fe85005a8be670a8bea
mathieu-palomba <mathieupalomba@msn.com>
2013-12-31 15:53:14 +0100

    Fist method for jsonp request.

--- js/controllers.js
+++ js/controllers.js
@@ -1,44 +1,88 @@
- /**
-  * Controllers.
-  */
- var phonecatControllers = angular.module('phonecatControllers', []);
+/**
+ * Controllers.
+ */
+var phonecatControllers = angular.module('phonecatControllers', []);
 
- /**
-  * Phone List controllers.
-  */
- phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
- 	function($scope, Phone) {
- 		// Method without our service
- 		/*$http.get('phones/phones.json').success(function(data) {
- 			$scope.phones = data;
- 		});*/
+/**
+ * Phone List controllers.
+ */
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone', 'Allocine',
+    function ($scope, Phone, Allocine) {
+        // Method without our service
+        /*$http.get('phones/phones.json').success(function(data) {
+         $scope.phones = data;
+         });*/
 
         // Method with our service
         $scope.phones = Phone.query();
 
         // This value must have the same name in the html view to set the default filter
- 		$scope.orderProp = 'age';
- 	}
- ]);
+        $scope.orderProp = 'age';
 
- /**
-  * Phone Details controllers.
-  */
- phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
- 	function($scope, $routeParams, Phone) {
+        $scope.page = Allocine.getPage('http://www.allocine.fr/recherche/?q=avatar');
+        //console.log($scope.page);
+    }
+]);
+
+/**
+ * Phone Details controllers.
+ */
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+    function ($scope, $routeParams, Phone) {
         // Method without our service
- 		/*$http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
- 			$scope.phone = data;
-            $scope.mainImageUrl = data.images[0];
- 		});*/
+        /*$http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+         $scope.phone = data;
+         $scope.mainImageUrl = data.images[0];
+         });*/
 
         // Method with our service
-        $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+        $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function (phone) {
             $scope.mainImageUrl = phone.images[0];
         });
 
-        $scope.setImage = function(imageUrl) {
+        $scope.setImage = function (imageUrl) {
             $scope.mainImageUrl = imageUrl;
         }
- 	}
- ]);
+    }
+]);
+
+/**
+ * Get phone informations.
+ */
+//phonecatControllers.controller('PhoneGetInformations', ['$scope',
+//    function ($scope) {
+//        $scope.getXhr = function () {
+//            var xhr = null;
+//            if (window.XMLHttpRequest) // Firefox et autres
+//                xhr = new XMLHttpRequest();
+//            else if (window.ActiveXObject) { // Internet Explorer
+//                try {
+//                    xhr = new ActiveXObject("Msxml2.XMLHTTP");
+//                } catch (e) {
+//                    xhr = new ActiveXObject("Microsoft.XMLHTTP");
+//                }
+//            }
+//            else { // XMLHttpRequest non supporté par le navigateur
+//                alert("Votre navigateur ne supporte pas les objets XMLHTTPRequest...");
+//                xhr = false;
+//            }
+//            return xhr
+//        };
+//
+//        $scope.getInformations = function() {
+//            var xhr = $scope.getXhr()
+//            // On défini ce qu'on va faire quand on aura la réponse
+//            xhr.onreadystatechange = function () {
+//                // On ne fait quelque chose que si on a tout reçu et que le serveur est ok
+//                if (xhr.readyState == 4 && xhr.status == 200) {
+//                    alert(xhr.responseText);
+//                }
+//            }
+//            xhr.open("GET", "http://www.allocine.fr/recherche/?q=avatar", true);
+//            xhr.send(null);
+//            console.log(xhr);
+//        };
+//
+//        $scope.getInformations;
+//    }]
+//);
--- js/services.js
+++ js/services.js
@@ -7,8 +7,95 @@ var phonecatServices = angular.module('phonecatServices', ['ngResource']);
  * This service permit to create a RESTful client and avoid the $http lower method.
  */
 phonecatServices.factory('Phone', ['$resource',
-    function($resource){
+    function ($resource) {
         return $resource('phones/:phoneId.json', {}, {
-            query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
+            query: {method: 'GET', params: {phoneId: 'phones'}, isArray: true}
         });
-    }]);
+    }]
+);
+
+phonecatServices.factory('Allocine', ['$http',
+    function ($http, $q) {
+        return {
+            getPage: function (page) {
+                //var deferred = $q.defer();
+
+//            $http.get(page).then(function(response) {
+//                console.log('response');
+//                console.log(response.data);
+//                //deferred.resolve(response.data);
+//            });
+
+//                $http.jsonp('http://www.allocine.fr/recherche/?q=avatar').
+//                    success(function(data, status, headers, config) {
+//                console.log('response');
+//                console.log(data);
+//                    }).
+//                    error(function(data, status, headers, config) {
+//                        console.log('error');
+//                        console.log(status);
+//                    });
+
+                /*var url = "http://www.omdbapi.com/?t=Avatar"; //"https://api.themoviedb.org/3/movie/550?api_key=37c2294ca3753bd14d165eda4b3f9314?callback";
+                $http.jsonp(url).then(function (response) {
+                    console.log('response');
+                    //var json = mapDOM(response.data, true);
+                    //console.log(json);
+                    //console.log(response.data);
+                });*/
+
+                function JSON_CALLBACK(data) {
+                    // returning from async callbacks is (generally) meaningless
+                    console.log("Success");
+                    console.log(data);
+                }
+                var url = "http://www.omdbapi.com/?t=Avatar&callback=JSON_CALLBACK";
+                $http.jsonp(url).
+                    success(function(data, status, headers, config) {
+                        // this callback will be called asynchronously
+                        // when the response is available
+                        console.log('response');
+                        console.log(data);
+                    });
+
+                /*$http({method: 'jsonp', url: "https://api.themoviedb.org/3/movie/550?api_key=37c2294ca3753bd14d165eda4b3f9314"}).
+                    success(function(data, status, headers, config) {
+                        // this callback will be called asynchronously
+                        // when the response is available
+                        console.log('response');
+                        console.log(data);
+                    }).
+                    error(function(data, status, headers, config) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                        console.log('error');
+                        console.log(data);
+                    });*/
+
+                /*var client = new XMLHttpRequest()
+                client.open("GET", "http://www.themoviedb.org/search?query=avatar")
+                client.onreadystatechange = function(data) {
+                    console.log('response');
+                    console.log(data);
+                }
+                client.send();*/
+
+                /*$.ajax({
+                        type: "GET",
+                        url: "http://www.themoviedb.org/search?query=avatar",
+                    crossDomain: true,
+                    //data: { ... },
+                    dataType: "json",
+
+                    error: function (xhr, status, error) {
+                        console.log('error');
+                    },
+                    success: function (json) {
+                        console.log('response');
+                    }});*/
+
+                return null;//deferred.promise;
+            }
+        };
+    }]
+);
