04ddea206db12995f4c2735710307fa70999156e
Mathieu PALOMBA <mathieupalomba@msn.com>
2013-12-31 17:01:53 +0100

    First version for jsonp request with "themoviedb.org" API.

--- js/controllers.js
+++ js/controllers.js
@@ -6,8 +6,8 @@ var phonecatControllers = angular.module('phonecatControllers', []);
 /**
  * Phone List controllers.
  */
-phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone', 'Allocine',
-    function ($scope, Phone, Allocine) {
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone', 'GetMovieData',
+    function ($scope, Phone, GetMovieData) {
         // Method without our service
         /*$http.get('phones/phones.json').success(function(data) {
          $scope.phones = data;
@@ -19,8 +19,11 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone', 'Allocine',
         // This value must have the same name in the html view to set the default filter
         $scope.orderProp = 'age';
 
-        $scope.page = Allocine.getPage('http://www.allocine.fr/recherche/?q=avatar');
-        //console.log($scope.page);
+        // This request get the movie ID to get the movie informations
+        $scope.page = GetMovieData.getData("https://api.themoviedb.org/3/search/movie?api_key=37c2294ca3753bd14d165eda4b3f9314&query=Avatar&language=fr&callback=JSON_CALLBACK");
+
+        // This request get the movie information from the previous request
+        $scope.page = GetMovieData.getData("https://api.themoviedb.org/3/movie/19995?api_key=37c2294ca3753bd14d165eda4b3f9314&language=fr&callback=JSON_CALLBACK");
     }
 ]);
 
--- js/services.js
+++ js/services.js
@@ -14,86 +14,23 @@ phonecatServices.factory('Phone', ['$resource',
     }]
 );
 
-phonecatServices.factory('Allocine', ['$http',
-    function ($http, $q) {
+phonecatServices.factory('GetMovieData', ['$http',
+    function ($http) {
         return {
-            getPage: function (page) {
-                //var deferred = $q.defer();
-
-//            $http.get(page).then(function(response) {
-//                console.log('response');
-//                console.log(response.data);
-//                //deferred.resolve(response.data);
-//            });
-
-//                $http.jsonp('http://www.allocine.fr/recherche/?q=avatar').
-//                    success(function(data, status, headers, config) {
-//                console.log('response');
-//                console.log(data);
-//                    }).
-//                    error(function(data, status, headers, config) {
-//                        console.log('error');
-//                        console.log(status);
-//                    });
-
-                /*var url = "http://www.omdbapi.com/?t=Avatar"; //"https://api.themoviedb.org/3/movie/550?api_key=37c2294ca3753bd14d165eda4b3f9314";
-                $http.jsonp(url).then(function (response) {
-                    console.log('response');
-                    //var json = mapDOM(response.data, true);
-                    //console.log(json);
-                    //console.log(response.data);
-                });*/
-
-                var url = "https://api.themoviedb.org/3/movie/550?api_key=37c2294ca3753bd14d165eda4b3f9314&language=fr&callback=JSON_CALLBACK";
+            getData: function (url) {
                 $http.jsonp(url).
-                    success(function(data, status, headers, config) {
+                    success(function (data, status, headers, config) {
                         // This callback will be called asynchronously when the response is available
-                        console.log('response');
+                        console.log('Response');
                         console.log(data);
                     }).
-                    error(function(data, status, headers, config) {
+                    error(function (data, status, headers, config) {
                         // Called asynchronously if an error occurs or server returns response with an error status.
-                        console.log('error');
+                        console.log('Error');
                         console.log(data);
                     });
 
-                /*$http({method: 'jsonp', url: "https://api.themoviedb.org/3/movie/550?api_key=37c2294ca3753bd14d165eda4b3f9314"}).
-                    success(function(data, status, headers, config) {
-                        // this callback will be called asynchronously
-                        // when the response is available
-                        console.log('response');
-                        console.log(data);
-                    }).
-                    error(function(data, status, headers, config) {
-                        // called asynchronously if an error occurs
-                        // or server returns response with an error status.
-                        console.log('error');
-                        console.log(data);
-                    });*/
-
-                /*var client = new XMLHttpRequest()
-                client.open("GET", "http://www.themoviedb.org/search?query=avatar")
-                client.onreadystatechange = function(data) {
-                    console.log('response');
-                    console.log(data);
-                }
-                client.send();*/
-
-                /*$.ajax({
-                        type: "GET",
-                        url: "http://www.themoviedb.org/search?query=avatar",
-                    crossDomain: true,
-                    //data: { ... },
-                    dataType: "json",
-
-                    error: function (xhr, status, error) {
-                        console.log('error');
-                    },
-                    success: function (json) {
-                        console.log('response');
-                    }});*/
-
-                return null;//deferred.promise;
+                return null;
             }
         };
     }]
